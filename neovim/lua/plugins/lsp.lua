e610bff7f5c9bd6bd1a85d9832b109571dabb0a9
Aleksey512 <yakunina1111@gmail.com>
2024-06-06 13:26:34 +0300

    neovim config add pythod venv support

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -1,6 +1,8 @@
 
 -- Setup language servers.
 local lspconfig = require('lspconfig')
+
+local py = require 'lang.python'
 lspconfig.pyright.setup {
 	settings = {
 	    pyright = {
@@ -9,11 +11,17 @@ lspconfig.pyright.setup {
 	    },
 	    python = {
 	      analysis = {
-	        -- Ignore all files for analysis to exclusively use Ruff for linting
+            -- Ignore all files for analysis to exclusively use Ruff for linting
 	        ignore = { '*' },
 	        },
 		},
-	},	
+	},
+    on_new_config = function(new_config, new_root_dir)
+        py.env(new_root_dir)
+        new_config.settings.python.pythonPath = vim.fn.exepath 'python'
+        -- new_config.cmd_env.PATH = py.env(new_root_dir) .. new_config.cmd_env.PATH
+        new_config.settings.python.analysis.extraPaths = { py.pep582(new_root_dir) }
+    end,
 }
 
 lspconfig.tsserver.setup({})
